43ee15c7c7f0bb1037987d7d6a53270c13c32d69
shiva shankar <sivas1475@gmail.com>
2022-12-28 04:28:03 +0530

    JENKINS

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 module "vpc" {
-  source = "git::https://github.com/DShankarGoud/tf-module-vpc.git"
+  source         = "git::https://github.com/DShankarGoud/tf-module-vpc.git"
   VPC_CIDR_BLOCK = var.VPC_CIDR_BLOCK
-  COMPONENT = var.COMPONENT
-  ENV = var.ENV
-  SUBNET_CIDR = var.SUBNET_CIDR
-  AZ = var.AZ
+  COMPONENT      = var.COMPONENT
+  ENV            = var.ENV
+  SUBNET_CIDR    = var.SUBNET_CIDR
+  AZ             = var.AZ
 }
 
 module "elasticache" {
   depends_on          = [module.vpc]
   source              = "git::https://github.com/DShankarGoud/tf-module-elasticache-redis.git"
-  ENGINE         = var.ENGINE
-  ENGINE_VERSION = var.ENGINE_VERSION
-  NODE_TYPE      = var.NODE_TYPE
-  ENV            = var.ENV
-  COMPONENT      = var.COMPONENT
-  NODE_COUNT     = var.NODE_COUNT
-  FAMILY         = var.FAMILY
+  ENGINE              = var.ENGINE
+  ENGINE_VERSION      = var.ENGINE_VERSION
+  NODE_TYPE           = var.NODE_TYPE
+  ENV                 = var.ENV
+  COMPONENT           = var.COMPONENT
+  NODE_COUNT          = var.NODE_COUNT
+  FAMILY              = var.FAMILY
 }
